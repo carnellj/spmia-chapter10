456606b677e2511c4f60e0cd81b9b8f9e270b7d8
david <dvriend2002@yahoo.com>
2019-08-03 09:07:20 -0500

    modify github url

--- travis_scripts/deploy_to_docker_hub.sh
+++ travis_scripts/deploy_to_docker_hub.sh
@@ -1,8 +1,8 @@
 echo "Pushing service docker images to docker hub ...."
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker push johncarnell/tmx-authentication-service:$BUILD_NAME
-docker push johncarnell/tmx-licensing-service:$BUILD_NAME
-docker push johncarnell/tmx-organization-service:$BUILD_NAME
-docker push johncarnell/tmx-confsvr:$BUILD_NAME
-docker push johncarnell/tmx-eurekasvr:$BUILD_NAME
-docker push johncarnell/tmx-zuulsvr:$BUILD_NAME
+docker push dvriend/tmx-authentication-service:$BUILD_NAME
+docker push dvriend/tmx-licensing-service:$BUILD_NAME
+docker push dvriend/tmx-organization-service:$BUILD_NAME
+docker push dvriend/tmx-confsvr:$BUILD_NAME
+docker push dvriend/tmx-eurekasvr:$BUILD_NAME
+docker push dvriend/tmx-zuulsvr:$BUILD_NAME
